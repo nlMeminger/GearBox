e786ebb0b1c75f301aeff2ec395987787aad74da
Red Ziogas <ajz1369@rit.edu>
2020-03-03 19:11:01 -0500

    Add pubsub functionality

--- app.py
+++ app.py
@@ -21,25 +21,31 @@ red = redis.StrictRedis() #Listen for redis events
 
 
 def event_stream():
-    pubsub = red.pubsub()
-    pubsub.subscribe('server_msg') #subscribe to run this function
-    for message in pubsub.listen():
-        print(message)
-        yield 'data: %s\n\n' % message['data'] #return a properly formatted server side event
+	pubsub = red.pubsub()
+	pubsub.subscribe('server_msg') #subscribe to server_msg stream
+	for message in pubsub.listen():
+		#print(message) #Debug
+		yield 'data: %s\n\n' % message['data'] #return a properly formatted server side event
+
+ 
+def sever_msg(data):
+	#Send a string to the server_msg stream
+	red.publish('server_msg', u'%s' % (data))
+
 
 @app.route("/stream")
 def stream():
 	#The event stream for the server side events
-    return flask.Response(event_stream(), mimetype="text/event-stream") #return the event stream with the proper mime so that chrome is happy
+	return flask.Response(event_stream(), mimetype="text/event-stream") #return the event stream with the proper mime so that chrome is happy
 
 
 @app.after_request
 def set_response_headers(response):
-    #This function runs after each request and tells the browser not to cache anything.
-    #Not going to waste data or cycles because it's local-only comms
-    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
-    response.headers['Pragma'] = 'no-cache'
-    response.headers['Expires'] = '0'
-    return response
+	#This function runs after each request and tells the browser not to cache anything.
+	#Not going to waste data or cycles because it's local-only comms
+	response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+	response.headers['Pragma'] = 'no-cache'
+	response.headers['Expires'] = '0'
+	return response
 
 app.run(host='0.0.0.0', port=8080) #Run the app on port 80
