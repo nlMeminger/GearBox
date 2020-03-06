538cd798c0f00706b84543c8cb6f789578344cff
Red Ziogas <ajz1369@rit.edu>
2020-03-06 12:20:42 -0500

    Create tab module

--- tabs/__init__.py
+++ tabs/__init__.py
@@ -1,7 +1,7 @@
 '''
 Tabs interface
 Module to handle loading and processing of tab modules
-Made by Red in 2018
+Made by Red in 2020
 Greetings, from the ROC!
 
 https://github.com/nlMeminger/MagiCarp
@@ -14,12 +14,24 @@ Or visit https://www.gnu.org/licenses/gpl-3.0.en.html
 import flask
 from flask import render_template
 
+import tabs.loader
+
 
 class tabHandler:
 	'''
 	Class to handle the initialization of tabs
 	'''
 
-	def __init__(self, config):
+	def __init__(self, config, app):
+		'''
+		Must pass the config file and the app instance to start the tab handler
+		'''
+		self.app = app
+		self.config = config
+
+		self.tabs = loader.getTabs(self.app, self.config)
+
+
+
 
 		
---
+++ tabs/settings.py
@@ -0,0 +1,21 @@
+'''
+Settings menu
+The settings menu for the car stereo project
+Made by Red in 2020
+Greetings, from the ROC!
+
+https://github.com/nlMeminger/MagiCarp
+
+This code is licensed under the GNU General Public License
+For more information see the LICENSE file that was distributed with this code
+Or visit https://www.gnu.org/licenses/gpl-3.0.en.html
+'''
+
+class register:
+
+	def __init__(self, config, app):
+		'''
+		Initialize the settings tab and all things related to it
+		'''
+		
+		pass
