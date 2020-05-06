b861c8ba8ef7804a531dc680ff9957d3aae2dbe3
Red Ziogas <ajz1369@rit.edu>
2020-05-06 00:50:49 -0400

    Standardize module system

--- tabs/__init__.py
+++ tabs/__init__.py
@@ -12,18 +12,35 @@ Or visit https://www.gnu.org/licenses/gpl-3.0.en.html
 '''
 
 import importlib
+import os
 import json
 
+modules = [] #Global modules list
 
 def load_tabs(config, app):
 
-	modules = []
+	global modules
+	
+	registered_pages = []
 
 	with open("MODULES.json") as f:
+		#Open the modules file
 		toImport = json.load(f)
 
 	for item in toImport["modules"]:
-		modules.append(importlib.import_module("tabs." + item)) #import the item from the string
-	
+		# Check duplicate names, as well as template filenames.
+
+		#Check for duplicate module names		
+		if item in modules:
+			raise DuplicateModuleError("Module " + item + " is already registered")
+		else:
+			modules.append(importlib.import_module("tabs." + item)) #import the item from the string
+			#Check for duplicate file names.
+			for file in os.listdir("tabs/" + item + "/templates"):
+				if file in registered_pages:
+					print("Page " + file + "in module " + item + " is already registered by another module. This may cause collision errors.")
+				else:
+					registered_pages.append(file)
+				
 	for item in modules:
 		app.register_blueprint(item.module)
--- tabs/settings/__init__.py
+++ tabs/settings/__init__.py
@@ -11,9 +11,65 @@ Or visit https://www.gnu.org/licenses/gpl-3.0.en.html
 '''
 
 from flask import Blueprint, render_template
+import json
+import redis
 
-module = Blueprint('settings', __name__, template_folder='html', static_folder='static')
+
+
+NAME = "settings"
+
+
+module = Blueprint(NAME, __name__, template_folder='templates', static_folder='static')
+red = redis.StrictRedis() #Listen for redis events
 
 @module.route("/settings")
 def show():
-	return render_template("settings.html")
+	#Show the tab. This should contain all the necessary setup before the template is rendered.
+	return render_template("settings.html", settings = settings)
+
+def hide():
+	#Hide the tab.
+	#Contains functions that the tab should run on switching to another tab.
+	#This should do stuff like closing open files, etc.
+	#This will be left as pass for most tabs.
+	pass
+
+def sever_msg(data):
+	#Send a string to the server_msg stream. This is used to send data to the client.
+	red.publish('server_msg', u'%s' % (data))
+
+def hide_tab(name):
+	#Hide another tab
+	#send "ALL" to hide all tabs.
+	red.publish('server_msg', name)
+
+def hide_stream(): 
+	#Subscribe to the hide stream
+	pubsub = red.pubsub()
+	pubsub.subscribe('hide') #subscribe to hide stream
+	for message in pubsub.listen():
+		if message == "ALL":
+			#Listen to the ALL stream
+			hide()
+		elif message == NAME:
+			hide()
+		
+
+#Arbitrary functions for this particular module
+def change_config(key, value):
+	#Change the config option key to the specified value.
+	#Writes directly to the config file.
+
+	with open("CONFIG.json", "w") as f:
+		configuration = json.load(f)
+		configuration[key] = value
+		json.dumps(configuration, f, indent=4)
+
+
+#Required functions to make the module run
+
+#Open the config.json file
+with open("CONFIG.json") as f:
+		settings = json.load(f)
+
+hide_stream() #Subscribe to the hide events stream.
